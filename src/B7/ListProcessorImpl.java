32dc3bd96fcac8898a8fc48e77b64fdcd326f4d1
unknown <vminh0227@gmail.com>
2025-03-26 11:58:40 +0700

    add

--- src/B7/ListProcessorImpl.java
+++ src/B7/ListProcessorImpl.java
@@ -4,17 +4,24 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class ListProcessorImpl {
+
     public static void main(String[] args) {
         List<Integer> numbers = List.of(5,-2,10,0,-7,-3);
 
         Predicate<Integer> isNegative = num -> num < 0;
 
-        ListProcessor processor = list -> list.stream().anyMatch(isNegative);
 
+        boolean hasNegative = false;
+        for (int num : numbers) {
+            if (isNegative.test(num)) {
+                hasNegative = true;
+                break;
+            }
+        }
         System.out.println("Danh sách số:");
         ListProcessor.printList(numbers);
 
-        boolean hasNegative = processor.containsNegative(numbers);
+
         if(hasNegative){
             System.out.println("Danh sách chứa số âm");
         }
